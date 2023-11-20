5f8c39ef1003905317c43d5a2e01d55855036672
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-11-20 07:11:35 +0700

    add auth login api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Resources\AuthLoginResources;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AuthController extends Controller
+{
+    public function AuthLogin (AuthLoginRequest $request)
+    {
+         // ! mendapatkan semua data
+         $data = $request->validated();
+         
+
+         $user = User::where('nama', $data['username'])->first();
+        //  dd($user);
+         if (!$user || !Hash::check($data['password'], $user->password)) {
+             throw new HttpResponseException(response([
+                 "errors" => [
+                     "username" => [
+                         "Username or password is incorrect"
+                     ]
+                 ]
+             ], 400));
+         }
+         // ! membuat token berdasarkan username
+         return response()->json(new AuthLoginResources($user), 200);
+    }
+}
