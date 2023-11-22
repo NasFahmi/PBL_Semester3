cdd388d806d584450d1453bd7b7c495c60c6f501
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-11-22 18:17:47 +0700

    fix prodcut controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,7 +47,7 @@ class ProductController extends Controller
         //     'stok' => 'required',
         //     'spesifikasi_product' => 'required',
 
-        ]);
+        // ]);
         $request->session()->put('product_data', [
             'nama_product' => $request->input('nama_product'),
             'harga_rendah' => $request->input('harga_rendah'),
