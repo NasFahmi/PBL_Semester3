d4e31f32cdf2b8f05b9661196cea4ea559632b24
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-11-29 21:24:53 +0700

    add information of detial transactioin

--- resources/views/pages/admin/transaksi/detail.blade.php
+++ resources/views/pages/admin/transaksi/detail.blade.php
@@ -3,6 +3,25 @@
 @section('content')
 <div class="container  px-6 pb-6 mx-auto">
     <h1 class="text-2xl my-6 font-semibold text-gray-700 ">Detail Transaksi</h1>
+    <p>Tanggal</p>
+    
+    <p>detail product yang dibeli</p>
+    <p>methode pembayaran</p>
+    <p>jumlah</p>
+    <p>total harga</p>
+    <p>keterangan</p>
+    <p>Status</p>
+    <p>Preorder atau tidak?</p>
+    <p>down payment</p>
+    <p>tanggal pembayaran preorder</p>
+    <p>tanggal pembayran down payment</p>
+    <p>kekurangan harga</p>
+
+    <p>informasi Pembeli</p>
+    <p>nama</p>
+    <p>email</p>
+    <p>alamat</p>
+    <p>no hp</p>
     
 </div>
 @endsection
