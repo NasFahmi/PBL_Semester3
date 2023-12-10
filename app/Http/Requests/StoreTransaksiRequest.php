7978521766b6e98a4650beb1a6a64e2f70730e33
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-12-10 12:45:01 +0700

    fix image in dashboard

--- app/Http/Requests/StoreTransaksiRequest.php
+++ app/Http/Requests/StoreTransaksiRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreTransaksiRequest extends FormRequest
 {
@@ -11,7 +13,8 @@ class StoreTransaksiRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        // return Auth::user() !== null;
+        return true;
     }
 
     /**
@@ -22,7 +25,27 @@ class StoreTransaksiRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'tanggal' => 'required',
+            'product' => 'required',
+            'methode_pembayaran' => 'required',
+            'jumlah' => 'required',
+            'total' => 'required',
+            'is_complete' => 'required',
+            'nama' => 'required',
+            'email' => 'required',
+            'alamat' => 'required',
+            'telepon' => 'required',
         ];
     }
+
+    public function messages(){
+        return [
+            'required' => 'The :attribute field is required.',
+        ];
+    }
+    function failedValidation(Validator $validator)  {
+        throw new HttpResponseException(response([
+            'errors'=>$validator->getMessageBag()
+        ]));
+    }   
 }
