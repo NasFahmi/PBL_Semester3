b73a5f6781d577ec3799c61c926ba2ba0c108177
afrix18 <deveroid389@gmail.com>
2023-12-20 23:44:58 +0700

    install spatie dan seeder user

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create (['name' => 'tambah-product']);
+        Permission::create(['name' => 'tambah-transaksi']);
+        Permission::create(['name' => 'tambah-preorder']);
+        Permission::create(['name' => 'edit-preorder']);
+        Permission::create(['name' => 'edit-transaksi']);
+        Permission::create(['name' => 'edit-product']);
+        Permission::create(['name' => 'cetak-transaksi']);
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'superadmin']);
+
+        $admin = Role::findByName('admin');
+        $admin->givePermissionTo('tambah-product');
+        $admin->givePermissionTo('tambah-transaksi');
+        $admin->givePermissionTo('tambah-preorder');
+
+        $superAdmin = Role::findByName('superadmin');
+        $superAdmin->givePermissionTo('edit-preorder');
+        $superAdmin->givePermissionTo('edit-transaksi');
+        $superAdmin->givePermissionTo('edit-product');
+        $superAdmin->givePermissionTo('cetak-transaksi');
+    }
+}
--- database/seeders/akunSeeder.php
+++ database/seeders/akunSeeder.php
@@ -7,21 +7,33 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-class akunSeeder extends Seeder
+class AkunSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::insert([
+        $admin = User::create([
             "nama" => "admin",
-            "email" => "pawonkoe@gmail.com",
+            "email" => "pawonkoeAdmin@gmail.com",
             "password" => Hash::make('admin'),
-            "link_toko" => "disana",
-            "link_wa" => "disini",
-            "link_ig" => "disani",
+            "link_toko" => "tidak punya",
+            "link_wa" => "tidak punya",
+            "link_ig" => "tidak punya",
             "alamat" => "sempu"
         ]);
+        $admin->assignRole('admin');
+
+        $owner = User::create([
+            "nama" => "pawonkoe",
+            "email" => "pawonkoe@gmail.com",
+            "password" => Hash::make('pawonkoe'),
+            "link_toko" => "ini toko",
+            "link_wa" => "ini wa",
+            "link_ig" => "ini ig",
+            "alamat" => "sempu"
+        ]);
+        $owner->assignRole('superadmin');
     }
 }
