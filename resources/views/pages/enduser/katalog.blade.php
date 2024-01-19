b7c327514c43011b14f1d26703c72345225afedd
afrix18 <deveroid389@gmail.com>
2024-01-19 22:44:34 +0700

    forgot search

--- resources/views/pages/enduser/katalog.blade.php
+++ resources/views/pages/enduser/katalog.blade.php
@@ -246,6 +246,20 @@
     {{-- <p>{{$data}}</p> --}}
     <div class=" mx-auto max-w-screen-xl px-4 mt-20 md:px-10 flex justify-center items-center flex-col ">
         <h1 class="text-3xl font-semibold text-center mb-10">Produk Kami</h1>
+         <label for="default-search" class="sr-only">Search</label>
+            <div class="relative w-2/4">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <form action="" method="GET">
+                    <input type="search" id="default-search" name="search"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Cari Produk">
+            </div>
         <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-center">
             @foreach ($data as $items)
                 @if ($items->stok > 0)
